78ac4be884be12f2df89e6c7209a4cd10e510e48
eliza <j.amelia.odell@gmail.com>
2018-02-25 00:03:37 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,31 @@
+{
+    var cuddlesArray = ["./dog/cuddles/cuddles1.JPG", "./cuddles2.JPG", "./dog/cuddles/cuddles3.JPG", "./dog/cuddles/cuddles4.JPG", "./dog/cuddles/cuddles5.JPG",
+    "./dog/cuddles/cuddles6.JPG", "./dog/cuddles/cuddles7.JPG", "./dog/cuddles/cuddles8.JPG", "./dog/cuddles/cuddles9.JPG", "./dog/cuddles/cuddles10.JPG",
+    "./dog/cuddles/cuddles11.JPG", "./dog/cuddles/cuddles12.JPG", "./dog/cuddles/cuddles13.JPG", "./dog/cuddles/cuddles14.JPG",
+    "./dog/cuddles/cuddles15.JPG", "./dog/cuddles/cuddles16.JPG", "./dog/cuddles/cuddles17.JPG", "./dog/cuddles/cuddles18.JPG", "./dog/cuddles/cuddles19.JPG",
+    "./dog/cuddles/cuddles20.JPG", "./dog/cuddles/cuddles21.JPG", "./dog/cuddles/cuddles22.JPG", "./dog/cuddles/cuddles23.JPG", "./dog/cuddles/cuddles24.JPG",
+    "./dog/cuddles/cuddles25.JPG", "./dog/cuddles/cuddles26.JPG", "./dog/cuddles/cuddles27.JPG"];
+    var shadowArray = ["./dog/shadow/shadow1.JPG", "./dog/shadow/shadow2.JPG", "./dog/shadow/shadow3.JPG", "./dog/shadow/shadow4.JPG","./dog/shadow/shadow5.JPG",
+    "./dog/shadow/shadow6.JPG", "./dog/shadow/shadow7.JPG", "./dog/shadow/shadow8.JPG", "./dog/shadow/shadow9.JPG","./dog/shadow/shadow10.JPG",
+    "./dog/shadow/shadow11.JPG", "./dog/shadow/shadow12.JPG", "./dog/shadow/shadow13.JPG", "./dog/shadow/shadow14.JPG", "./dog/shadow/shadow15.JPG",
+    "./dog/shadow/shadow16.JPG", "./dog/shadow/shadow17.JPG"];
+    var baileyArray = ["./dog/bailey/bailey1.JPG", "./dog/bailey/bailey2.JPG"];
+    var zoeArray = [];
+    var dixieArray = ["./dog/dixie/dixie1.JPG"];
+    var heroArray = [];
+    var nessieArray = ["./cat/nessie/nessie1.JPG", "./cat/nessie/nessie2.JPG", "./cat/nessie/nessie3.JPG"];
+    var peteArray = ["./cat/pete/pete1.JPG"];
+    var nashArray = ["./cat/nash/nash1.JPG", "./cat/nash/nash2.JPG"];
+    var meowArray = ["Mee-wow!", "Meeeeeeeeeeow.", "Mee-yip!", "Mrrow?", "Mew!", "Meow meow meow meow meow meow meow meow!"];
+
+    var cuddles = cuddlesArray[Math.floor(Math.random() * cuddlesArray.length)];
+    var shadow = shadowArray[Math.floor(Math.random() * shadowArray.length)];
+    var bailey = baileyArray[Math.floor(Math.random() * baileyArray.length)];
+    var zoe = zoeArray[Math.floor(Math.random() * zoeArray.length)];
+    var dixie = dixieArray[Math.floor(Math.random() * dixieArray.length)];
+    var hero = heroArray[Math.floor(Math.random() * heroArray.length)];
+    var nessie = nessieArray[Math.floor(Math.random() * nessieArray.length)];
+    var pete = peteArray[Math.floor(Math.random() * peteArray.length)];
+    var nash = nashArray[Math.floor(Math.random() * nashArray.length)];
+    var meow = meowArray[Math.floor(Math.random() * meowArray.length)];
+}
